6f0c0706f638184aa0c140198825e79d732944fc
R1shem <Kaeler2007@gmail.com>
2026-08-28 19:13:36 +0500

    Обновлены права доступа на учащённое FPS для адекватного теста перед релизом на других устройствах.

--- MaksLifeInChat/App.xaml.cs
+++ MaksLifeInChat/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace MaksLifeInChat
@@ -9,7 +10,22 @@ namespace MaksLifeInChat
     /// </summary>
     public partial class App : Application
     {
-        public static MainWindow mainWindow;
+        public static MainWindow mainWindow; 
+        [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+        private static extern uint TimeBeginPeriod(uint uPeriod);
+
+        [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+        private static extern uint TimeEndPeriod(uint uPeriod);
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            TimeBeginPeriod(1);
+            base.OnStartup(e);
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            TimeEndPeriod(1);
+            base.OnExit(e);
+        }
     }
 
 }
